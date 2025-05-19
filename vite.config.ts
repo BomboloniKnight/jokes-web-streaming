357762e1763bf3b5ce330658c7afe47c0e29aa7f
Muhammad Islami Pasha <158245614+BomboloniKnight@users.noreply.github.com>
2025-05-19 10:48:17 +0700

    Set up HTTPS for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: true
+    https: {
+      key: fs.readFileSync('./.certificates/localhost-key.pem'),
+      cert: fs.readFileSync('./.certificates/localhost.pem'),
+    },
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
