964dafbbd3bed21271b2ab9a2a65bd8d21cb619d
Muhammad Islami Pasha <158245614+BomboloniKnight@users.noreply.github.com>
2025-05-19 10:49:13 +0700

    Remove HTTPS configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('./.certificates/localhost-key.pem'),
-      cert: fs.readFileSync('./.certificates/localhost.pem'),
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
